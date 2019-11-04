99f7c9b4a5fd2225960371da46ed34948e098f44
Mdjiseaps1996 <salaichitoolatt1996@outlook.com>
2019-11-04 13:45:26 +0630

    category and role directory create

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -99,6 +99,7 @@ class AdminController extends Controller
 
     public function showRoleCreateForm()
     {
-        return view('admin.register-role');
+        return view('admin.role.register-role');
     }
+    
 }
